3cf9028a88f8e34800a06137ecb6000731d0eeed
Willam Wang <tengwang@zwdai.com>
2018-08-01 14:42:17 +0800

    优化逻辑 & 日志

--- main.js
+++ main.js
@@ -30,39 +30,41 @@ if(Array.isArray(spiderUrl)&&spiderUrl.length>0){
     var spiderCircle = setInterval(function(){
         var index=urlIndex;
         urlIndex++;
-        if(urlIndex > spiderUrl.length){
+        if(index >= spiderUrl.length){
             clearInterval(spiderCircle);
-            console.log("用时: "+new Date() - time);
-            page.close();   //关闭网页
-            phantom.exit();   //退出phantomjs命令行
-        }
-        page.open(spiderUrl[index], function(status) {
-            if(status == 'fail'){
-                console.log(index,"spider: "+spiderUrl[index],"fail");
-                // 对失败页面再次爬取，尝试三次
-                if(failog.count<3){ 
-                    urlIndex--;
+            console.log("用时: ",new Date() - time);
+            // page.close();   //关闭网页
+            // phantom.exit();   //退出phantomjs命令行
+        }else{
+            console.log(index);
+            page.open(spiderUrl[index], function(status) {
+                if(status == 'fail'){
+                    console.log("fail ==>>> spider: "+spiderUrl[index]);
+                    // 对失败页面再次爬取，尝试三次
+                    if(failog.count<3){ 
+                        urlIndex--;
+                    }
+                    if(spiderUrl[index]==failog.url){
+                        failog.count+=1;
+                    }else{
+                        failog.url=spiderUrl[index];
+                        failog.count=1;
+                    }
+                    return;
                 }
-                if(spiderUrl[index]==failog.url){
-                    failog.count+=1;
-                }else{
-                    failog.url=spiderUrl[index];
-                    failog.count=1;
-                }
-                return;
-            }
-            console.log(index,"spider: "+spiderUrl[index],"success");
-            var title = page.evaluate(function() {   //可执行js操作
-                window.scrollTo(0,document.body.scrollHeight);
-                return document.title;
+                console.log("success ==>>> spider: "+spiderUrl[index]);
+                var title = page.evaluate(function() {   //可执行js操作
+                    window.scrollTo(0,document.body.scrollHeight);
+                    return document.title;
+                });
+                page.render('get/'+title+'.png');   //生成html页面图片
+                console.log('page title is ' + title);
+                var file = fs.open('get/'+title+'.html', 'a');
+                file.write(page.content);   //保存html
+                file.close();
             });
-            page.render('get/'+title+'.png');   //生成html页面图片
-            console.log('==>>> page title is ' + title);
-            var file = fs.open('get/'+title+'.html', 'a');
-            file.write(page.content);   //保存html
-            file.close();
-        });
-    },3000);  
+        }
+    },2000);  
 }else{
     console.log('无可用spiderUrl，請檢查配置項');
     phantom.exit();
